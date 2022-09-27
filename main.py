4023b13d56df7e92039407ad6ca41c4b691a2934
EthanA11 <111260137+EthanAlorico@users.noreply.github.com>
2022-09-27 20:36:46 +0000

    Gave up on tkinter and coming back to normal quiz

---
+++ main.py
@@ -0,0 +1,51 @@
+print("Welcome to the Common knowledge quiz")
+
+playing = input("This Quiz has 10 questions which needs common knowledge of English, Science and geography are you ready to take the quiz?")
+
+if playing != "yes":
+  quit()
+
+print("The Quiz will now start")
+
+answer = input("Question 1) What is the Capital city of australia?")
+if answer == "Canberra":
+  print("Correct")
+
+else: print("Incorrect, the correct answer is Canberra")
+
+answer = input("Question 2) In what Country is the Mountain Mt.Fuji Located?")
+if answer == "Japan":
+  print("Correct")
+
+else: print("Incorrect, the correct answer is Japan")
+
+answer = input("Question 3) How many Bones are in a human body?")
+if answer == "206":
+  print("Correct")
+
+else: print("Incorrect, the correct amount is 206")
+
+answer = input("Question 4) What Shape is the Earth?")
+if answer == "Sphere":
+  print("Correct")
+
+else: print("Incorrect, The correct answer is Sphere")
+
+answer = input("Question 5) How many days are there in a year?")
+if answer == "365":
+  print("Correct")
+
+else: print("Incorrect, the correct answer is 365")
+
+answer = input("Question 6) What is the Elements Sodium and Chlorine combine to make?")
+if answer == "Salt":
+  print("Correct")
+
+else: print("Incorrect, the correct answer is Salt")
+
+answer = input("Question 7) What is the Smalest Country?")
+if answer == "Vatican City":
+  print("Correct")
+
+else: print("Incorrect the correct answer is Vatican City")
+
